053313821ab1bf15973f45bdacb5e0fb26e0f655
ueda.keisuke <ueda.keisuke@me.com>
2012-07-23 17:49:53 -0300

    Update RotatingBot.cpp

--- RotatingBot.cpp
+++ RotatingBot.cpp
@@ -24,8 +24,8 @@ typedef long long ll;
 class RotatingBot {
 public:
     int minArea( vector <int> moves )
-    {
-        int x = 0;
+	{
+		int x = 0;
         int y = 0;
         
         int max_x = moves[0];
@@ -73,106 +73,91 @@ public:
             if( x > max_x || x < min_x || y > max_y || y < min_y )
                 return -1;
             
-            if( ( x == max_x || x == min_x ) && ( y == max_y || y == min_y ) )
-            {
-                // OK
-            }
-            else
+            if( ( x != max_x && x != min_x ) || ( y != max_y && y != min_y ) )
             {
                 point next = make_pair(x + d_x[i % 4], y + d_y[i % 4]);
-                if( s.count(next) != 0 )
+                if( s.count(next) == 0 && i != moves.size() - 1 )
                 {
-                    // OK
+                    return -1;
                 }
-                else
-                {
-                    if( i == moves.size() - 1 )
-                    {
-                        
-                    }
-                    else
-                    {
-                        return -1;
-                    }
-                }
+                
             }
-            
         }
         
         return ( max_x - min_x + 1 ) * ( max_y - min_y + 1 );
-    }
+	}
 };
 
 // BEGIN CUT HERE
 namespace moj_harness {
-    int run_test_case(int);
-    void run_test(int casenum = -1, bool quiet = false) {
-        if (casenum != -1) {
-            if (run_test_case(casenum) == -1 && !quiet) {
-                cerr << "Illegal input! Test case " << casenum << " does not exist." << endl;
-            }
-            return;
-        }
-        
-        int correct = 0, total = 0;
-        for (int i=0;; ++i) {
-            int x = run_test_case(i);
-            if (x == -1) {
-                if (i >= 100) break;
-                continue;
-            }
-            correct += x;
-            ++total;
-        }
-        
-        if (total == 0) {
-            cerr << "No test cases run." << endl;
-        } else if (correct < total) {
-            cerr << "Some cases FAILED (passed " << correct << " of " << total << ")." << endl;
-        } else {
-            cerr << "All " << total << " tests passed!" << endl;
-        }
-    }
+	int run_test_case(int);
+	void run_test(int casenum = -1, bool quiet = false) {
+		if (casenum != -1) {
+			if (run_test_case(casenum) == -1 && !quiet) {
+				cerr << "Illegal input! Test case " << casenum << " does not exist." << endl;
+			}
+			return;
+		}
+		
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = run_test_case(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+		
+		if (total == 0) {
+			cerr << "No test cases run." << endl;
+		} else if (correct < total) {
+			cerr << "Some cases FAILED (passed " << correct << " of " << total << ")." << endl;
+		} else {
+			cerr << "All " << total << " tests passed!" << endl;
+		}
+	}
+	
+	int verify_case(int casenum, const int &expected, const int &received, clock_t elapsed) { 
+		cerr << "Example " << casenum << "... "; 
+		
+		string verdict;
+		vector<string> info;
+		char buf[100];
+		
+		if (elapsed > CLOCKS_PER_SEC / 200) {
+			sprintf(buf, "time %.2fs", elapsed * (1.0/CLOCKS_PER_SEC));
+			info.push_back(buf);
+		}
+		
+		if (expected == received) {
+			verdict = "PASSED";
+		} else {
+			verdict = "FAILED";
+		}
+		
+		cerr << verdict;
+		if (!info.empty()) {
+			cerr << " (";
+			for (int i=0; i<(int)info.size(); ++i) {
+				if (i > 0) cerr << ", ";
+				cerr << info[i];
+			}
+			cerr << ")";
+		}
+		cerr << endl;
+		
+		if (verdict == "FAILED") {
+			cerr << "    Expected: " << expected << endl; 
+			cerr << "    Received: " << received << endl; 
+		}
+		
+		return verdict == "PASSED";
+	}
     
-    int verify_case(int casenum, const int &expected, const int &received, clock_t elapsed) { 
-        cerr << "Example " << casenum << "... "; 
-        
-        string verdict;
-        vector<string> info;
-        char buf[100];
-        
-        if (elapsed > CLOCKS_PER_SEC / 200) {
-            sprintf(buf, "time %.2fs", elapsed * (1.0/CLOCKS_PER_SEC));
-            info.push_back(buf);
-        }
-        
-        if (expected == received) {
-            verdict = "PASSED";
-        } else {
-            verdict = "FAILED";
-        }
-        
-        cerr << verdict;
-        if (!info.empty()) {
-            cerr << " (";
-            for (int i=0; i<(int)info.size(); ++i) {
-                if (i > 0) cerr << ", ";
-                cerr << info[i];
-            }
-            cerr << ")";
-        }
-        cerr << endl;
-        
-        if (verdict == "FAILED") {
-            cerr << "    Expected: " << expected << endl; 
-            cerr << "    Received: " << received << endl; 
-        }
-        
-        return verdict == "PASSED";
-    }
-    
-    int run_test_case(int casenum) {
-        switch (casenum) {
+	int run_test_case(int casenum) {
+		switch (casenum) {
             case 0: {
                 int moves[]               = {1,2,3,4,5};
                 int expected__            = -1;
@@ -266,21 +251,21 @@ namespace moj_harness {
                  }*/
             default:
                 return -1;
-        }
-    }
+		}
+	}
 }
 
 
 int main(int argc, char *argv[])
 {
     moj_harness::run_test(0);
-    return 0;
+ //   return 0;
     
-    if (argc == 1) {
-        moj_harness::run_test();
-    } else {
-        for (int i=1; i<argc; ++i)
-            moj_harness::run_test(atoi(argv[i]));
-    }
+	if (argc == 1) {
+		moj_harness::run_test();
+	} else {
+		for (int i=1; i<argc; ++i)
+			moj_harness::run_test(atoi(argv[i]));
+	}
 }
 // END CUT HERE
